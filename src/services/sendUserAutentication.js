c2ba2295f9fbbb9be3480a6c83f04b29ec80ff8d
marco <marco.prina01@universitadipavia.it>
2020-04-07 15:57:02 +0200

    Ora raccolgo gli errori quando chiamo questa funzione da un altra parte

--- src/services/sendUserAutentication.js
+++ src/services/sendUserAutentication.js
@@ -4,12 +4,10 @@ export default {
   postRegisterUser(payload) {
     return api.post(`v1/account/customer`, payload)
           .then(response => response)
-          .catch(error => error.response)
   },
   signUser(payload) {
     return api.post('v1/account/login', payload)
         .then(response => response)
-        .catch(error => error.response)
 
   }
 }
