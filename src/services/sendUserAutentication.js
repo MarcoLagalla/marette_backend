5786cbc185658f06cc2b10864d5c9f8ea6c32198
marco <marco.prina01@universitadipavia.it>
2020-04-07 18:50:11 +0200

    Aggiunge header per autorizazione

--- src/services/sendUserAutentication.js
+++ src/services/sendUserAutentication.js
@@ -3,11 +3,18 @@ import api from '@/services/api'
 export default {
   postRegisterUser(payload) {
     return api.post(`v1/account/customer`, payload)
-          .then(response => response)
+          .then(response => {
+            api.defaults.headers.common['Authorization'] = response.data.token
+            return response
+          })
+
   },
   signUser(payload) {
     return api.post('v1/account/login', payload)
-        .then(response => response)
+        .then(response => {
+            api.defaults.headers.common['Authorization'] = response.data.token
+            return response
+          })
 
   },
   logout() {
