12b5238dbe28be0d05553c55a2974c194a075185
Dominik Lietavec <dddomk11@gmail.com>
2020-04-08 19:00:45 +0200

    >:(

---
+++ src/views/login/Index.vue
@@ -0,0 +1,31 @@
+<script>
+  // Extensions
+  import View from '@/views/View'
+
+  // Mixins
+  import LoadSections from '@/mixins/load-sections'
+
+  export default {
+    name: 'Login',
+
+    metaInfo: { title: 'Login' },
+
+    extends: View,
+
+    mixins: [
+      LoadSections([
+        'logform',
+        'affiliates',
+        'newsletter',
+        'info',
+      ]),
+    ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'login',
+      },
+    },
+  }
+</script>
