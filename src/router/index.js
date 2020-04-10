4b76534553b5c020652e09f286b613047efa3e24
Dominik Lietavec <dddomk11@gmail.com>
2020-04-10 11:15:51 +0200

    sjkdn

--- src/router/index.js
+++ src/router/index.js
@@ -73,6 +73,7 @@ const router = new Router({
           path: 'registration',
           name: 'Registration',
           component: () => import('@/views/pages/registration.vue'),
+          beforeEnter: ifNotAuthenticated,
           //meta: { src: require('@/assets/.jpg') },
         },
         {
