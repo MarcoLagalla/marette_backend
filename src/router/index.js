692ac4156e4dfdb5fff7f2730c0b730e1cb80951
marco <marco.prina01@universitadipavia.it>
2020-04-13 16:49:19 +0200

    Non puoi accedere a login se sei già loggato

--- src/router/index.js
+++ src/router/index.js
@@ -74,7 +74,7 @@ const router = new Router({
           path: 'login',
           name: 'Login',
           component: () => import('@/views/pages/login.vue'),
-          //beforeEnter: ifNotAuthenticated,
+          beforeEnter: ifNotAuthenticated,
         },
         {
           path: '*',
