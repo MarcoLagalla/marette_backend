94048ce05d46cb2bd32639cde46354e360c64d83
Marco Lagalla <marcolagalla@gmail.com>
2020-04-16 16:26:17 +0200

    fix commit cache

--- backend/account/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from backend.account.models import Customer, Business
-# Register your models here.
-
-admin.site.register(Customer)
-admin.site.register(Business)
--- backend/webapp/views.py
+++
@@ -1,3 +0,0 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import TemplateView, View, CreateView, ListView
-from backend.account.models import BusinessUser
