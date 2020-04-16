20a351a4e91bea99f0de1f7fefb2672143467d3a
Dominik Lietavec <dddomk11@gmail.com>
2020-04-16 17:06:59 +0200

    restcards

--- src/components/base/CardR.vue
+++ src/components/base/CardR.vue
@@ -39,7 +39,7 @@
 
 <script>
   export default {
-    name: 'BaseCard',
+    name: 'BaseCardR',
 
     data: () => ({
 
--- src/views/sections/RestaurantCards.vue
+++ src/views/sections/RestaurantCards.vue
@@ -1,30 +1,58 @@
 <template>
   <base-section id="theme-features">
-    <base-section-heading title="Ristoranti">
-     Ecco la nostra scelta di ristoranti
-    </base-section-heading>
+  <base-section-heading title="Ristoranti">
+  Ecco la nostra scelta di ristoranti
+</base-section-heading>
 
-    <v-container>
-      <v-row>
-        <v-col
-          v-for="(restaurant, i) in restaurantList"
-          :key="i"
-          cols="12"
-          md="6"
-        >
-          <base-avatar-card
-            v-bind="restaurant"
-            align="left"
-            horizontal
-            size="96"
-          >
-            <h3>{{restaurant.activity_name}}</h3>
-            <p>{{ restaurant.activity_description}}</p>
-          </base-avatar-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </base-section>
+<v-container>
+  <v-row>
+    <v-col
+    v-for="(restaurant, i) in restaurantList"
+    :key="i"
+    cols="12"
+    md="6"
+    >
+    <v-card
+    v-bind="restaurant"
+    class="mx-auto"
+    max-width="400"
+    >
+    <v-img
+    class="white--text align-end"
+    height="200px"
+    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+    <v-card-title>{{ restaurant.activity_name }}</v-card-title>
+  </v-img>
+
+  <v-card-subtitle class="pb-0">Category</v-card-subtitle>
+
+  <v-card-text class="text--primary">
+    <div>{{ restaurant.activity_description }}</div>
+
+
+  </v-card-text>
+
+  <v-card-actions>
+    <v-btn
+    color="orange"
+    text
+    >
+    Share
+  </v-btn>
+
+  <v-btn
+  color="orange"
+  text
+  >
+  Explore
+</v-btn>
+</v-card-actions>
+</v-card>
+</v-col>
+</v-row>
+</v-container>
+</base-section>
 </template>
 
 <script>
