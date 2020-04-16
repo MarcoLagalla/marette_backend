75008432eaf01f8c107979e7899e7448329c2077
Giuseppe <giudisp@gmail.com>
2020-04-16 18:07:57 +0200

    Aggiunta: sezione per gestire il cambio password, funzione per gestire l'update dei cookies, varaibile errors per la gestione degli eventuali errori.

--- src/store/modules/userProfile.js
+++ src/store/modules/userProfile.js
@@ -1,46 +1,77 @@
 /* eslint-disable no-unused-vars */
-import getUserProfile from "../../services/getUserProfile"
+import manageUserProfile from "../../services/manageUserProfile"
 import sendUserAuthentication from "../../services/sendUserAuthentication";
 
 
 const state = {
-  id: '',
-  username: '',
-  email: '',
-  first_name: "",
-  last_name: "",
-  birth_date: "",
-  phone: "",
-  is_superuser: false,
-  status: ''
+  user:{
+
+    id: '',
+    username: '',
+    email: '',
+    first_name: "",
+    last_name: "",
+    birth_date: "",
+    phone: "",
+    is_superuser: false,
+
+  },
+
+  errors: [],
+  status: '',
+
 }
 
 const getters = {
-  isSuperuser: state => state.is_superuser,
+ /* isSuperuser: state => state.is_superuser,
   id: state => state.id,
-  username: state => state.username,
+  username: state => state.username,*/
 
+  user: state => state.user,
+  errors: state=> state.errors,
 }
 
 const actions = {
+
+
   getUserData: ({commit, dispatch}, id) => {
     commit('USER_REQUEST')
-    getUserProfile.getUserProfile(id)
-    .then(resp => {
-      const data = resp.data
-      data.id = id
-      commit('USER_SUCCESS', data)
-    })
-    .catch(err => {
-      dispatch("userAuthentication/logout", null,  { root: true });
-      commit('USER_ERROR');
-    })
+    manageUserProfile.getUserProfile(id)
+        .then(resp => {
+          const data = resp.data
+          data.id = id
+          commit('USER_SUCCESS', data)
+        })
+        .catch(err => {
+          dispatch("userAuthentication/logout", null, {root: true});
+          commit('USER_ERROR');
+        })
   },
 
   logout: ({commit}) => {
     commit('USER_PROF_LOGOUT')
-  }
+  },
 
+  changePassword: ({commit}, data) => {
+    return new Promise((resolve, reject) => {
+      commit('USER_REQUEST')
+
+      manageUserProfile.changePassword(state.user.id, data)
+          .then(resp => {
+
+            commit('PSW_CHANGE_SUCCESS', resp.data.password)
+            updateCookie(resp.data)
+            resolve(resp.data.password)
+
+          })
+          .catch(err => {
+
+            commit('PSW_CHANGE_ERROR', err.response)
+            reject(err)
+
+          })
+    })
+  }
 }
 
 const mutations = {
@@ -50,30 +81,40 @@ const mutations = {
 
   USER_SUCCESS: (state, data) => {
     state.status = 'success'
-    state.username = data.username
-    state.id = data.id
-    state.email = data.email
-    state.first_name = data.first_name
-    state.last_name = data.last_name
-    state.birth_date = data.birth_date
-    state.cellphone_number = data.phone
-    state.is_superuser = data.is_superuser
+    state.user.username = data.username
+    state.user.id = data.id
+    state.user.email = data.email
+    state.user.first_name = data.first_name
+    state.user.last_name = data.last_name
+    state.user.birth_date = data.birth_date
+    state.user.phone = data.phone
+    state.user.is_superuser = data.is_superuser
+  },
+
+  PSW_CHANGE_SUCCESS: (state) => {
+    state.status = 'success'
+  },
+
+  PSW_CHANGE_ERROR: (state, error) => {
+    state.status = 'error'
+    state.errors = error.data
   },
 
   USER_PROF_LOGOUT: (state) => {
     state.status = ''
-    state.username =  ''
-    state.id =  ''
-    state.email =  ''
-    state.first_name =  ''
-    state.last_name =  ''
-    state.birth_date = ''
-    state.cellphone_number =  ''
-    state.is_superuser =  ''
+    state.user.username =  ''
+    state.user.id =  ''
+    state.user.email =  ''
+    state.user.first_name =  ''
+    state.user.last_name =  ''
+    state.user.birth_date = ''
+    state.user.phone =  ''
+    state.user.is_superuser =  ''
   },
 
   USER_ERROR: (state) => {
     state.status = 'error'
+
   },
 }
 
@@ -83,4 +124,11 @@ export default {
   getters,
   actions,
   mutations
+}
+function updateCookie( data ) {
+  var d = new Date();
+  var exdays = 364;
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = "user-token=" + data.token + ";" + expires + ";path=/";//TODO: flaggare il cookie come sicuro solo quando avremo https
 }
