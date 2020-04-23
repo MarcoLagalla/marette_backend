4a51780d3dffd35dc8b84224188bc1e5ca14ed42
Giuseppe <giudisp@gmail.com>
2020-04-23 13:54:35 +0200

    Aggiunta pagina Reset Password

---
+++ src/views/pages/resetPass.vue
@@ -0,0 +1,36 @@
+
+<script>
+      // Extensions
+        import View from '@/views/View'
+
+      // Mixins
+        import LoadSections from '@/mixins/load-sections'
+
+    export default {
+        name: "resetPass",
+
+        metaInfo: { title: 'resetPass' },
+
+        extends: View,
+
+    mixins: [
+        LoadSections([
+            'ResetPass',
+            'info',
+         ]),
+    ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'resetPass',
+      },
+    },
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
