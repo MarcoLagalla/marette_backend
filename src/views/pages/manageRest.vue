65254f0dfd6c10c3ebc822a2d996bcd59b89f5f0
Giuseppe <giudisp@gmail.com>
2020-04-25 12:04:25 +0200

    aggiunta pagona gestione del ristorante ( NON FUNZIONA )

---
+++ src/views/pages/manageRest.vue
@@ -0,0 +1,36 @@
+
+<script>
+      // Extensions
+        import View from '@/views/View'
+
+      // Mixins
+        import LoadSections from '@/mixins/load-sections'
+
+    export default {
+        name: "manageRest",
+
+        metaInfo: { title: 'manageRest' },
+
+        extends: View,
+
+    mixins: [
+        LoadSections([
+            'ManageRest',
+            'info',
+         ]),
+    ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'manageRest',
+      },
+    },
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/sections/ManageRest.vue
@@ -0,0 +1,63 @@
+<template>
+
+    <button type="button" > Aggiungi Prodotto</button>
+    <v-form>
+                    <v-text-field
+                    solo background-color="#486F83"
+                    v-model='name'
+                    type="text"
+                    placeholder=" Inserire nome prodotto"
+                    id="name"
+                    name="name" required></v-text-field>
+
+                    <v-text-field
+                    solo background-color="#486F83"
+                    v-model='description'
+                    type="text"
+                    placeholder=" Inserire descrizione"
+                    id="description"
+                    name="description" required></v-text-field>
+
+                    <v-text-field
+                    solo background-color="#486F83"
+                    v-model='category'
+                    type="text"
+                    placeholder=" Inserire categoria"
+                    id="category"
+                    name="category" required></v-text-field>
+
+                    <v-text-field
+                    solo background-color="#486F83"
+                    v-model='price'
+                    type="text"
+                    placeholder=" Inserire prezzo"
+                    id="price"
+                    name="price" required></v-text-field>
+    </v-form>
+    <p v-for="(campo, i) in user" :key="i" >{{i.replace(/_/g , ' ')}} : {{campo}}</p>
+
+    
+</template>
+
+<script>
+    export default {
+        name: "ManageRest",
+
+        data() {
+
+            return{
+
+            }
+        },
+
+        computed() {
+            food_category_choice(){
+                return this.$store.getters['manageRestaurant/food_category_choice']
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
