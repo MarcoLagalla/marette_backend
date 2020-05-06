25aea1a3ef0505eba21cb1d9e2863f89fbe8cd3e
Giuseppe <giudisp@gmail.com>
2020-05-06 12:15:18 +0200

    Aggiunti tag funzionanti e Discounts

--- src/components/base/AddProduct.vue
+++ src/components/base/AddProduct.vue
@@ -26,72 +26,216 @@
           name="price"
           required
         ></v-text-field>
-        <picture-input
-          ref="productImage"
-          @change="onChanged"
-          :width="300"
-          :height="300"
-          :crop="true"
-          :changeOnClick="false"
-          accept="image/jpeg, image/png, image/gif"
-          buttonClass="ui button primary"
-          :customStrings="{
+              <v-btn @click="toggleShowTags">Inserisci i tag</v-btn>
+        <br>
+          <v-card
+            v-show="showTags"
+            class="mx-auto"
+            max-width="500"
+          >
+            <v-list shaped>
+              <v-list-item-group
+                v-model="selectedTags"
+                multiple
+              >
+                <template v-for="(item, i) in tags">
+                  <v-divider
+                    v-if="!item"
+                    :key="`divider-${i}`"
+                  ></v-divider>
+
+                  <v-list-item
+                    v-else
+                    :key="`item-${i}`"
+                    :value="item.id"
+                    active-class="blue--text text--accent-4"
+                  >
+                    <template v-slot:default="{ active, toggle }">
+                      <v-list-item-content>
+                        <v-list-item-title v-text="item.name" ></v-list-item-title>
+                      </v-list-item-content>
+
+                      <v-list-item-action>
+                        <v-checkbox
+                          :input-value="active"
+                          :true-value="item"
+                          color="blue accent-4"
+                          @click="toggle"
+                        ></v-checkbox>
+                      </v-list-item-action>
+                    </template>
+                  </v-list-item>
+                </template>
+              </v-list-item-group>
+            </v-list>
+            </v-card>
+
+        <br><br>
+      <v-btn @click="toggleShowDiscounts">Inserisci uno sconto al prodotto</v-btn>
+        <br>
+          <v-card
+            v-show="showDiscounts"
+            class="mx-auto"
+            max-width="500"
+          >
+            <v-list shaped>
+              <v-list-item-group
+                v-model="selectedDiscounts"
+                multiple
+              >
+                <template v-for="(item, i) in discounts">
+                  <v-divider
+                    v-if="!item"
+                    :key="`divider-${i}`"
+                  ></v-divider>
+
+                  <v-list-item
+                    v-else
+                    :key="`item-${i}`"
+                    :value="item.id"
+                    active-class="blue--text text--accent-4"
+                  >
+                    <template v-slot:default="{ active, toggle }">
+                      <v-list-item-content>
+                        <v-list-item-title v-text="item.name" ></v-list-item-title>
+                      </v-list-item-content>
+
+                      <v-list-item-action>
+                        <v-checkbox
+                          :input-value="active"
+                          :true-value="item"
+                          color="blue accent-4"
+                          @click="toggle"
+                        ></v-checkbox>
+                      </v-list-item-action>
+                    </template>
+                  </v-list-item>
+                </template>
+              </v-list-item-group>
+            </v-list>
+            </v-card>
+
+        <br><br>
+
+      <picture-input
+        ref="productImage"
+        @change="onChanged"
+        :width="300"
+        :height="300"
+        size="5"
+        :crop="true"
+        :changeOnClick="false"
+        accept="image/jpeg, image/png, image/gif"
+        buttonClass="ui button primary"
+        :customStrings="{
         upload: '<h1>Carica immagine</h1>',
-        drag: 'Trascina qui la tua immagine o clicca per selezionarla'}"
-        ></picture-input>
-        <button type="submit" class="addconf">Aggiungi Prodotto</button>
-      </v-form>
+        drag: 'Trascina qui la tua immagine o clicca per selezionarla'}">
+      </picture-input>
+        <br><br>
+
+
+     <button type="submit" class="addconf">Aggiungi Prodotto</button>
+
+    </v-form>
+                    <!--vue-anka-cropper></vue-anka-cropper-->
+
     </div>
+
+
   </div>
 </template>
 <script>
-import PictureInput from "vue-picture-input";
-import { mapActions } from "vuex";
+  import {mapActions} from "vuex";
+  //import vueAnkaCropper from 'vue-anka-cropper';
+  import PictureInput from "vue-picture-input";
+
+// DATA URI TO FILE FORSE PUò essere usato per passare da URI a file object
+
+
 export default {
   name: "AddProduct",
-  props: ["category"],
+  props: ['category'],
 
   components: {
-    PictureInput
+    // vueAnkaCropper,
+    PictureInput,
+
+
   },
 
   data() {
     return {
-      name: "",
-      description: "",
-      price: "",
-      image: ""
-    };
+        name: '',
+        description: '',
+        price: '',
+        image: '',
+        showTags: false,
+        showDiscounts: false,
+        selectedTags: [],
+        selectedDiscounts: [],
+
+    }
   },
   computed: {
     food_category_choice() {
-      return this.$store.getters["manageRestaurant/food_category_choice"];
-    }
-  },
-  methods: {
-    ...mapActions("restaurantData", ["addProduct"]),
-    onChanged() {
-      console.log("New picture loaded");
-      if (this.$refs.productImage.file) {
-        this.image = this.$refs.productImage.file;
-      } else {
-        console.log("Old browser. No support for Filereader API");
-      }
+      return this.$store.getters['manageRestaurant/food_category_choice']
     },
-    submitProduct: function() {
-      const data = {
-        name: this.name,
-        description: this.description,
-        category: this.category,
-        price: this.price
-      };
-      const formData = new FormData();
-      formData.append("image", this.image);
-      formData.append("data", JSON.stringify(data));
-      this.addProduct(formData);
-    }
-  }
-};
+    discounts() {
+      return this.$store.getters['manageRestaurant/discounts']
+    },
+
+    tags() {
+      return this.$store.getters['restaurantData/tags']
+    },
+  },
+
+
+    created() {
+      this.$store.dispatch("restaurantData/getListTag")
+      this.$store.dispatch("restaurantData/getListDiscounts")
+    },
+
+
+
+  methods: {
+      ...mapActions('restaurantData', ['addProduct']),
+      ...mapActions('restaurantData',['listTag']),
+      onChanged() {
+        console.log("New picture loaded");
+        if (this.$refs.productImage.file) {
+          this.image = this.$refs.productImage.file;
+        } else {
+          console.log("Old browser. No support for Filereader API");
+        }
+      },
+      submitProduct: function() {
+          const data = {
+              "name": this.name,
+              "description": this.description,
+              "category": this.category,
+              "price": this.price,
+              "tags": this.selectedTags
+
+          };
+         const formData = new FormData();
+          formData.append('image', this.image);
+          formData.append('data', JSON.stringify(data));
+          this.addProduct(formData)
+        },
+
+      toggleShowTags() {
+        this.showTags = !this.showTags;
+      },
+
+    toggleShowDiscounts() {
+        this.showDiscounts = !this.showDiscounts;
+      },
+
+
+  },
+
+
+}
 </script>
 <style scoped>
 .addprod {
