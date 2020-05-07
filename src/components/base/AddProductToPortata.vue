e1928d07d7ea28f155dfaf8c66efec27a9d69169
MarcoPrina <marco.prina01@universitadipavia.it>
2020-05-07 11:59:24 +0200

    Aggiunta scelta categoria

--- src/components/base/AddProductToPortata.vue
+++ src/components/base/AddProductToPortata.vue
@@ -1,11 +1,33 @@
 <template>
-    <p>prodotto</p>
+  <div>
+    <select v-model="prova">
+      <option v-for="category in categories" :key="category" >{{category}}</option>
+    </select>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "AddProductToPortata"
+  export default {
+    name: "AddProductToPortata",
+    data: () => ({
+      prova: ""
+    }),
+    computed: {
+      menu() {
+        return this.$store.getters['restaurantData/productList']
+      },
+      categories() {
+        var cat = []
+        var men = this.$store.getters['restaurantData/productList']
+        Object.keys(men).forEach(function(key) {
+          if(men[key].length!==0){
+            cat.push(key)
+          }
+        });
+        return cat
+      }
     }
+  }
 </script>
 
 <style scoped>
