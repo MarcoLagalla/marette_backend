97b7453396e09c00ecd3063f9086cdac665ad2cd
marco <marco.prina01@universitadipavia.it>
2020-05-07 21:51:52 +0200

    Aggiunge un prodotto alla portata

--- src/components/base/AddPortata.vue
+++ src/components/base/AddPortata.vue
@@ -2,27 +2,57 @@
   <form>
     <v-text-field v-model='name' type="text" label="Nome della portata" required></v-text-field>
     <v-text-field v-model='num_products' type="number" label="Numero di piatti selezionabili" required></v-text-field>
-    <v-btn @click="addProduct" text>Aggiungi prodotto</v-btn>
-    <base-add-product-to-portata v-for="product in products" :key="product">{{product}}</base-add-product-to-portata>
+
+    <v-row >
+        <v-col v-for="(product, i) in products" :key="i" cols="12" >
+          <v-card color="#616161" dark class="product">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div class="quant">
+                <div class>
+                  <v-avatar class="ma-3" size="100" tile>
+                    <v-img :src="product.image"></v-img>
+                  </v-avatar>
+                </div>
+                <div class>
+                  <v-card-title class="headline" v-text="product.name"></v-card-title>
+                  <v-card-subtitle class="pb-0" v-text="product.category"></v-card-subtitle>
+                  <div class="description" v-text="product.description"></div>
+                </div>
+              </div>
+              <div class="pos2" v-for="(item, j) in product.tags" :key="j" v-text="item.name"></div>
+              <v-card-actions class="pos1">
+                <div class="quant">
+                  <div v-text="product.price"></div>
+                  <v-icon small class="quant">fas fa-euro-sign</v-icon>
+                </div>
+                <v-btn color="red">
+                  <i class="fas fa-shopping-basket">Elimina</i>
+                </v-btn>
+              </v-card-actions>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+
+    <v-btn @click="showAdd=true" text>Aggiungi prodotto</v-btn>
+    <base-add-product-to-portata v-show="showAdd" @added_product="addProduct($event)"></base-add-product-to-portata>
+
   </form>
 </template>
 
 <script>
   export default {
     name: "AddPortata",
-    data: () => ({
+      data: () => ({
       name: '',
       num_products: 1,
-      products: [],
+      showAdd: false,
+      products: []
     }),
     methods: {
-      addProduct: function() {
-        if (this.products.length === 0){
-          this.products.push(0)
-        }
-        else {
-          this.products.push( this.products[this.products.length - 1] + 1)
-        }
+      addProduct: function(prod) {
+        this.products.push(prod);
+        this.showAdd =  false
       }
     }
   }
--- src/components/base/AddProductToPortata.vue
+++ src/components/base/AddProductToPortata.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-select :items="categories" label="Categoria del prodotto:" v-model="category" @change="choosedCategory = true"></v-select>
-    <v-select :items="menu[category]" item-text="name" item-value="id" label="Selezionare prodotto:" v-model="product" :disabled="!choosedCategory"></v-select>
-    <v-btn :disabled="product===''" @click="$emit('addProduct', product)">Aggiungi</v-btn>
+    <v-select :items="menu[category]" item-text="name" item-value="id" label="Selezionare prodotto:" v-model="productId" :disabled="!choosedCategory"></v-select>
+    <v-btn :disabled="productId===''" @click="added_product">Aggiungi</v-btn>
   </div>
 </template>
 
@@ -12,8 +12,19 @@
     data: () => ({
       category: "",
       choosedCategory: false,
-      product: ''
+      productId: ''
     }),
+    methods: {
+      added_product: function() {
+        var prod = {}
+        const prodId = this.productId
+        this.menu[this.category].forEach(function (product) {
+          if (product.id === prodId)
+            prod = product
+        });
+        this.$emit('added_product', prod)
+      }
+    },
     computed: {
       menu() {
         return this.$store.getters['restaurantData/productList']
