a90e7f9db8a964e6e09b81746da6e4e2681e5ebd
MarcoPrina <marco.prina01@universitadipavia.it>
2020-05-08 13:02:19 +0200

    create base per portata e product

--- src/components/base/Portata.vue
+++ src/components/base/Portata.vue
@@ -1,18 +1,30 @@
 <template>
   <v-card color="mx-auto" width="800" height="800">
     <v-card-title class="headline" v-text="portata.name"></v-card-title>
-    <v-card-subtitle
-            v-text="'Puoi scegliere ' + portata.num_products + ' di questi prodotti'">
-    </v-card-subtitle>
-    <v-btn @click="addPortata" text>Aggiungi portata</v-btn>
-    <base-add-portata @added_portata="submitPortata($event)"></base-add-portata>
+    <v-card-subtitle v-text="'Puoi scegliere ' + portata.num_products + ' di questi prodotti:'"> </v-card-subtitle>
+    <base-product v-for="product in portata.products" :key="product.id" :product="product"></base-product>
+    <v-card-actions class="pos1">
+      <v-btn v-if="this.delete" color="red" @click="$emit('removed')">
+        <i class="fas fa-shopping-basket">Elimina</i>
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
     export default {
         name: "Portata",
-        props: ['portata']
+        props:{
+          portata: {
+            type: Object,
+            required: true
+          },
+          delete: {
+            type: Boolean,
+            required: false,
+            default: false
+          },
+        },
     }
 </script>
 
