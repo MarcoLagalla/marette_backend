80f16cbf64415f13d78c4d1808fa7db64bee30cd
MarcoPrina <marco.prina01@universitadipavia.it>
2020-05-08 11:54:46 +0200

    Creata base per portata così posso usarla comodamente

---
+++ src/components/base/Portata.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card color="mx-auto" width="800" height="800">
+    <v-card-title class="headline" v-text="portata.name"></v-card-title>
+    <v-card-subtitle
+            v-text="'Puoi scegliere ' + portata.num_products + ' di questi prodotti'">
+    </v-card-subtitle>
+    <v-btn @click="addPortata" text>Aggiungi portata</v-btn>
+    <base-add-portata @added_portata="submitPortata($event)"></base-add-portata>
+  </v-card>
+</template>
+
+<script>
+    export default {
+        name: "Portata",
+        props: ['portata']
+    }
+</script>
+
+<style scoped>
+
+</style>
