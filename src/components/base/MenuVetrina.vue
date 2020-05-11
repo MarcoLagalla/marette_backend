08845ea8e169a822224c0f8266444243f5f8fca1
marco <marco.prina01@universitadipavia.it>
2020-05-11 22:50:09 +0200

    Inutile mandare menù se è un props...

--- src/components/base/MenuVetrina.vue
+++ src/components/base/MenuVetrina.vue
@@ -21,7 +21,7 @@
       <v-btn v-if="!admin" @click="$emit('added')" class="addtocart">
         Aggiungi al carrello <i class="fas fa-shopping-basket"></i>
       </v-btn>
-      <v-btn name="delete" v-if="admin" @click="$emit('removed', menu)" class="managebutton">
+      <v-btn name="delete" v-if="admin" @click="$emit('removed')" class="managebutton">
         Elimina Menù <i class="fas fa-times"></i>
       </v-btn>
       <v-btn name="delete" v-if="admin" @click="$emit('edited')" class="managebutton">
