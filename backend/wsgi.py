eb049e72fcd1a1c5d221e35850f2802f4c7d8084
Marco Lagalla <marcolagalla@gmail.com>
2020-05-14 20:25:21 +0200

    dev

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 # This will set production as default, but we must still set it with an
 # ENV on heroku to ensure that the migrate command runs agains the correct DB
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
 
 application = get_wsgi_application()
 
