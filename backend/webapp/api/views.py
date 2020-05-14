70012d755949def9388b7b0ea3f1102690a80c73
Michele <michelereina@live.it>
2020-05-14 22:12:43 +0200

    added paginator feature to SearchRestaurantAPIView()

--- backend/webapp/api/views.py
+++ backend/webapp/api/views.py
@@ -1,5 +1,5 @@
 import json
-
+import django
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
@@ -16,6 +16,9 @@ from ..models.models import Restaurant
 from ..models.components import RestaurantComponents
 from ...account.models import Business
 from ...account.permissions import IsBusiness
+from rest_framework.utils.urls import remove_query_param, replace_query_param
+from django.core.paginator import Paginator
+
 import phonenumbers
 import re
 from localflavor.it.util import vat_number_validation
@@ -138,7 +141,7 @@ class UpdateRestaurantAPIView(APIView):
 class SearchRestaurantAPIView(APIView):
     permission_classes = [AllowAny]
 
-    def post(self, request):
+    def get(self, request):
         queried_name = None
         queried_city = None
         restaurant = []
@@ -155,23 +158,88 @@ class SearchRestaurantAPIView(APIView):
         try:
             if queried_name and queried_city:
                 restaurant = Restaurant.objects.filter(city__icontains=queried_city).filter(
-                    activity_name__icontains=queried_name, activity_description__icontains=queried_name)
+                    activity_name__icontains=queried_name, activity_description__icontains=queried_name).order_by('-id')
 
             elif queried_name:
                 restaurant = Restaurant.objects.filter(activity_name__icontains=queried_name,
-                                                             activity_description__icontains=queried_name)
+                                                             activity_description__icontains=queried_name).order_by('-id')
             elif queried_city:
-                restaurant = Restaurant.objects.filter(city__icontains=queried_city)
+                restaurant = Restaurant.objects.filter(city__icontains=queried_city).order_by('-id')
             else:
 
                 return Response({'error': ["Non hai specificato alcun filtro."]},
                                 status.HTTP_400_BAD_REQUEST)
 
-            serializer = ListRestaurantSerializer(instance=restaurant, many=True)
-            data = serializer.data
-
         except Restaurant.DoesNotExist:
             return Response({'error': ["Nessun Ristorante trovato che rispecchia i filtri specificati."]},
                             status.HTTP_404_NOT_FOUND)
 
-        return Response(data, status.HTTP_200_OK)
+        # -----------------------------------------------------------
+        page_number = request.query_params.get('page_number', 1)
+        page_size = request.query_params.get('page_size', 1)
+
+        try:
+            paginator = Paginator(restaurant, page_size)
+            page = paginator.page(page_number)
+        except django.core.paginator.EmptyPage:
+            page = paginator.page(1)
+
+        serializer = ListRestaurantSerializer(page, many=True, context={'request': request})    #instance=page
+        # -----------------------------------------------------------
+
+        navigator = NavigationLinks(self.request, paginator, page_number)
+
+        data = {
+            'first': navigator.get_first_link(),
+            'previous': navigator.get_previous_link(),
+            'next': navigator.get_next_link(),
+            'last': navigator.get_last_link()
+        }
+
+        data.update({'results': serializer.data})
+
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class NavigationLinks:
+
+    paginator = None
+    page_number = None
+    request = None
+
+    def __init__(self, request, paginator, page_number):
+
+        self.request = request
+        self.paginator = paginator
+        self.page_number = page_number
+
+    def get_first_link(self):
+        # page_number is the current page
+        if int(self.page_number) == 1:
+            return None
+        url = self.request.build_absolute_uri()
+        return replace_query_param(url, 'page_number', 1)
+
+    def get_last_link(self):
+        if int(self.page_number) == self.paginator.num_pages:
+            return None
+        url = self.request.build_absolute_uri()
+        return replace_query_param(url, 'page_number', self.paginator.num_pages)
+
+    def get_previous_link(self):
+        url = self.request.build_absolute_uri()
+        page = self.paginator.page(self.page_number)
+        if page.has_previous():
+            url = replace_query_param(url, 'page_number', page.previous_page_number())
+        else:
+            url = None
+        return url
+
+    def get_next_link(self):
+        url = self.request.build_absolute_uri()
+        page = self.paginator.page(self.page_number)
+        if page.has_next():
+            url = replace_query_param(url, 'page_number', page.next_page_number())
+        else:
+            url = None
+        return url
