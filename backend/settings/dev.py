a66425ab7e96294cb71be78a951af6cf9afd68b8
Marco Lagalla <marcolagalla@gmail.com>
2020-05-16 10:26:45 +0200

    added django-ses

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -175,13 +175,13 @@ PHONENUMBER_DB_FORMAT = 'NATIONAL'
 PHONENUMBER_DEFAULT_REGION = 'IT'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'marette.dev@gmail.com'  # config('EM_ACCOUNT')
-EMAIL_HOST_PASSWORD = 'marette123'  # config('EM_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_HOST_USER = 'marette.dev@gmail.com'  # config('EM_ACCOUNT')
+# EMAIL_HOST_PASSWORD = 'marette123'  # config('EM_PASSWORD')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 ADMIN_REORDER = (
--- backend/webapp/admin.py
+++ backend/webapp/admin.py
@@ -14,35 +14,6 @@ from .models.orders import Order
 class RestaurantAdmin(admin.ModelAdmin):
     readonly_fields = ('url', 'slug', )
 
-    def save_model(self, request, obj, form, change):
-        super(RestaurantAdmin, self).save_model(request, obj, form, change)
-        obj.set_url()
-        restaurant = obj
-
-        try:
-            r_components = RestaurantComponents.objects.all().get(restaurant=restaurant)
-        except RestaurantComponents.DoesNotExist:
-            # create ComponentsPanels (empty)
-            home = HomeComponent.objects.create(restaurant=restaurant, name='HOME')
-            vetrina = VetrinaComponent.objects.create(restaurant=restaurant, name='VETRINA')
-            galleria = GalleriaComponent.objects.create(restaurant=restaurant, name='GALLERIA')
-            eventi = EventiComponent.objects.create(restaurant=restaurant, name='EVENTI')
-            menu = MenuComponent.objects.create(restaurant=restaurant, name='MENU')
-            contattaci = ContattaciComponent.objects.create(restaurant=restaurant, name='CONTATTACI')
-
-            RestaurantComponents.objects.create(
-                restaurant=restaurant,
-                home=home,
-                vetrina=vetrina,
-                galleria=galleria,
-                eventi=eventi,
-                menu=menu,
-                contattaci=contattaci
-            )
-
-        super(RestaurantAdmin, self).save_model(request, obj, form, change)
-
-
 admin.site.register(Restaurant, RestaurantAdmin)
 
 
@@ -81,6 +52,7 @@ class MenuInline(django.forms.ModelForm):
         for entry in obj.entries.all():
             entry.delete()
         return super(MenuInline, self).delete_model(request, obj)
+
 
 class MenuAdmin(ModelAdmin):
   form = MenuInline
