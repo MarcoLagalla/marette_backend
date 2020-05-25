28dc134fa9cfe03e43a6d1af43344621716111a9
Michele <michelereina@live.it>
2020-05-25 18:29:47 +0200

    NavigationLinks moved into a dedicated files utils.py in the root of backend

--- backend/orders/api/views.py
+++ backend/orders/api/views.py
@@ -16,6 +16,7 @@ from ..models import OrderNotification
 from .serializers import OrderNotificationSerialzier
 
 from django.core.paginator import Paginator
+from ...utils import NavigationLinks
 
 
 class AllNotifications(APIView):
@@ -55,50 +56,6 @@ class AllNotifications(APIView):
         data.update({'results': serializer.data})
 
         return Response(data, status=status.HTTP_200_OK)
-
-
-class NavigationLinks:
-
-    paginator = None
-    page_number = None
-    request = None
-
-    def __init__(self, request, paginator, page_number):
-
-        self.request = request
-        self.paginator = paginator
-        self.page_number = page_number
-
-    def get_first_link(self):
-        # page_number is the current page
-        if int(self.page_number) == 1:
-            return None
-        url = self.request.build_absolute_uri()
-        return replace_query_param(url, 'page_number', 1)
-
-    def get_last_link(self):
-        if int(self.page_number) == self.paginator.num_pages:
-            return None
-        url = self.request.build_absolute_uri()
-        return replace_query_param(url, 'page_number', self.paginator.num_pages)
-
-    def get_previous_link(self):
-        url = self.request.build_absolute_uri()
-        page = self.paginator.page(self.page_number)
-        if page.has_previous():
-            url = replace_query_param(url, 'page_number', page.previous_page_number())
-        else:
-            url = None
-        return url
-
-    def get_next_link(self):
-        url = self.request.build_absolute_uri()
-        page = self.paginator.page(self.page_number)
-        if page.has_next():
-            url = replace_query_param(url, 'page_number', page.next_page_number())
-        else:
-            url = None
-        return url
 
 
 class UnReadCount(APIView):
