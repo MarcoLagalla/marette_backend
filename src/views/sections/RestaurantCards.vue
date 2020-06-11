ac97092542db2d1c5f6f3a04a62c9eb58c6a7436
Dominik Lietavec <dddomk11@gmail.com>
2020-06-11 13:17:56 +0200

    modificate restaurant cards

--- src/views/sections/RestaurantCards.vue
+++ src/views/sections/RestaurantCards.vue
@@ -9,31 +9,38 @@
                 <div class="divider"></div>
                 <span class="subt"> Ecco la nostra scelta di ristoranti</span></div>
 
-            <v-container>
+            <div>
                 <v-row>
-                    <v-col v-for="(restaurant, i) in restaurantList" :key="i" cols="12" sm="6" md="4" lg="4">
+                    <v-col v-for="(restaurant, i) in restaurantList" :key="i" cols="12" sm="6" md="4" lg="3">
                         <router-link :to="restaurant.url">
-                            <div v-bind="restaurant" class="restcard">
-                                <v-img class="white--text align-end imag" height="200px"
-                                       :src="restaurant.image">
-                                    <v-card-title><h2>{{ restaurant.activity_name }}</h2></v-card-title>
-                                </v-img>
-
-
-                                <div class="actions">
-                                    <button class="share">
-                                        Condividi
-                                        <v-icon color="var(--ming)"> mdi-share-variant</v-icon>
-                                    </button>
-                                    <button class="like">
-                                        <v-icon color="var(--ming)"> mdi-heart</v-icon>
-                                    </button>
+                            <div v-bind="restaurant" >
+                                <div>
+                                    <div class="example-2 card">
+                                        <div class="wrapper">
+                                            <div class="header">
+                                                <div class="date">
+                                                    <span class="author">categoria?</span>
+                                                </div>
+                                                <ul class="menu-content">
+                                                        <li><a class="fas fa-heart"><span>18</span></a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="data">
+                                                <div class="content">
+                                                    <span class="author">categoria?</span>
+                                                    <h1 class="title"><a href="#">{{restaurant.activity_name}}</a></h1>
+                                                    <p class="text">{{restaurant.activity_description}}</p>
+                                                    <a href="#" class="button">Entra nel negozio</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </router-link>
                     </v-col>
                 </v-row>
-            </v-container>
+            </div>
         </base-section>
     </div>
 </template>
@@ -54,6 +61,11 @@
     }
 </script>
 <style scoped>
+    * {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        box-sizing: border-box;
+    }
     .body {
         /*background: linear-gradient(to bottom, #aaffa9, #11ffbd)!important;*/
         background: var(--whitesmoke);
@@ -76,32 +88,6 @@
         text-align: center;
     }
 
-    .actions {
-        padding: 10px;
-    }
-
-    .like {
-        float: right;
-        padding: 10px;
-        transition: 0.3s ease-in-out;
-        opacity: 0.8;
-
-    }
-
-    .share {
-        font-weight: bold;
-        color: var(--ming);
-        border-radius: 25px;
-        padding: 10px;
-        font-size: 1em;
-
-        transition: 0.3s ease-in-out;
-    }
-
-    .share:hover {
-        color: var(--ming);
-        opacity: 1;
-    }
 
     .restcard {
         width: 80%;
@@ -148,5 +134,170 @@
     .description {
         overflow: hidden!important;
         height: 50px;
+    }
+
+
+
+    .card {
+        float: left;
+        padding: 0 1.7rem;
+        width: 100%;
+    }
+    .card .menu-content {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+    .card .menu-content::before, .card .menu-content::after {
+        content: '';
+        display: table;
+    }
+    .card .menu-content::after {
+        clear: both;
+    }
+    .card .menu-content li {
+        display: inline-block;
+    }
+    .card .menu-content a {
+        color: #fff;
+    }
+    .card .menu-content span {
+        position: absolute;
+        left: 50%;
+        top: 0;
+        font-size: 10px;
+        font-weight: 700;
+        font-family: 'Open Sans';
+        -webkit-transform: translate(-50%, 0);
+        transform: translate(-50%, 0);
+    }
+    .card .wrapper {
+        background-color: #fff;
+        min-height: 440px;
+        max-width: 300px;
+        position: relative;
+        overflow: hidden;
+        box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.2);
+    }
+    .card .wrapper:hover .data {
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+    }
+    .card .data {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        -webkit-transform: translateY(calc(70px + 1em));
+        transform: translateY(calc(70px + 1em));
+        -webkit-transition: -webkit-transform 0.3s;
+        transition: -webkit-transform 0.3s;
+        transition: transform 0.3s;
+        transition: transform 0.3s, -webkit-transform 0.3s;
+    }
+    .card .data .content {
+        padding: 1em;
+        position: relative;
+        z-index: 1;
+    }
+    .card .author {
+        font-size: 12px;
+        text-transform: capitalize;
+        text-shadow: 0 0 1px black;
+    }
+    .card .title {
+        margin-top: 10px;
+        font-family: "Open Sans", sans-serif;
+        font-weight: 300;
+        font-size: 2rem!important;
+        text-shadow: 0 0 1px black;
+    }
+    .card .text {
+        height: 70px;
+        margin: 0;
+        background: rgba(0,0,0,0.0);
+    }
+    .card input[type='checkbox'] {
+        display: none;
+    }
+    .card input[type='checkbox']:checked + .menu-content {
+        -webkit-transform: translateY(-60px);
+        transform: translateY(-60px);
+    }
+    .example-2 .wrapper {
+        background: url("../../assets/tempcards.jpg") center/cover no-repeat;
+    }
+    .example-2 .wrapper:hover .menu-content span {
+        -webkit-transform: translate(-50%, -10px);
+        transform: translate(-50%, -10px);
+        opacity: 1;
+    }
+    .example-2 .wrapper:hover .data div {
+        background: rgba(0,0,0,0.5);
+        transition: 0.4s ease-in-out;
+    }
+    .example-2 .wrapper .data div {
+        transition: 0.4s ease-in-out;
+    }
+    .example-2 .header {
+        color: #fff;
+        padding: 1em;
+    }
+    .example-2 .header::before, .example-2 .header::after {
+        content: '';
+        display: table;
+    }
+    .example-2 .header::after {
+        clear: both;
+    }
+    .example-2 .header .date {
+        float: left;
+        font-size: 12px;
+    }
+    .example-2 .menu-content {
+        float: right;
+    }
+    .example-2 .menu-content li {
+        margin: 0 5px;
+        position: relative;
+    }
+    .example-2 .menu-content span {
+        -webkit-transition: all 0.3s;
+        transition: all 0.3s;
+        opacity: 0;
+    }
+    .example-2 .data {
+        color: #fff;
+        -webkit-transform: translateY(calc(70px + 4.5em));
+        transform: translateY(calc(70px + 4.5em));
+    }
+    .example-2 .title a {
+        color: #fff;
+    }
+    .example-2 .button {
+        display: block;
+        width: 100px;
+        margin: 2em auto 1em;
+        text-align: center;
+        font-size: 12px;
+        color: #fff;
+        line-height: 1;
+        position: relative;
+        font-weight: 700;
+    }
+    .example-2 .button::after {
+        content: '\2192';
+        opacity: 0;
+        position: absolute;
+        right: 0;
+        top: 50%;
+        -webkit-transform: translate(0, -50%);
+        transform: translate(0, -50%);
+        -webkit-transition: all 0.3s;
+        transition: all 0.3s;
+    }
+    .example-2 .button:hover::after {
+        -webkit-transform: translate(5px, -50%);
+        transform: translate(5px, -50%);
+        opacity: 1;
     }
 </style>
