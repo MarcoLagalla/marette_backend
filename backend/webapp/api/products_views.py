ccaa4b481ca4ae5bce8066e10ec75becdfaeb2cd
Marco Lagalla <marco.lagalla01@universitadipavia.it>
2020-06-11 20:10:18 +0200

    added product_id

--- backend/webapp/api/products_views.py
+++ backend/webapp/api/products_views.py
@@ -90,6 +90,7 @@ class AddProduct(APIView):
                         ret_data = {}
                         product = serializer.save(restaurant)
                         ret_data.update(serializer.data)
+                        ret_data.update({'id': product.id})
                         ret_data.update({'image': product.get_image()})
                         ret_data.update({'thumb_image': product.get_thumb_image()})
                         return Response(ret_data, status=status.HTTP_201_CREATED)
