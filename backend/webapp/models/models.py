d5022b5c669c51490ce299e4fada18bebd28237f
Marco Lagalla <marcolagalla@gmail.com>
2020-06-12 20:12:31 +0200

    fixed placeholder for products

--- backend/webapp/models/models.py
+++ backend/webapp/models/models.py
@@ -247,18 +247,18 @@ class Product(models.Model):
     def get_thumb_image(self):
         if not (self.thumb_image and hasattr(self.thumb_image, 'url')):
             try:
-                return FOOD_CATEGORY_CHOICES_THUMBS_IMAGES.get(self.category)
+                return settings.MEDIA_URL + FOOD_CATEGORY_CHOICES_THUMBS_IMAGES.get(self.category)
             except KeyError:
-                return FOOD_CATEGORY_CHOICES_THUMBS_IMAGES.get('Altro')
+                return settings.MEDIA_URL + FOOD_CATEGORY_CHOICES_THUMBS_IMAGES.get('Altro')
         else:
             return self.thumb_image.url
 
     def get_image(self):
         if not (self.image and hasattr(self.image, 'url')):
             try:
-                return FOOD_CATEGORY_CHOICES_IMAGES.get(self.category)
+                return settings.MEDIA_URL + FOOD_CATEGORY_CHOICES_IMAGES.get(self.category)
             except KeyError:
-                return FOOD_CATEGORY_CHOICES_IMAGES.get('Altro')
+                return settings.MEDIA_URL + FOOD_CATEGORY_CHOICES_IMAGES.get('Altro')
         else:
             return self.image.url
 
