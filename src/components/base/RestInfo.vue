3da90cd460fe60fd60253ec736f57486552afa88
Marco Lagalla <marcolagalla@gmail.com>
2020-06-15 15:23:05 +0200

    added map

--- src/components/base/RestInfo.vue
+++ src/components/base/RestInfo.vue
@@ -22,28 +22,67 @@
                     </div>
                </v-col>
                <v-col cols="12" md="4">
-
-                        <gmaps-map :options="mapOptions" />
-
+                    <div id="map" style="width: 450px; height: 250px;"></div>
                </v-col>
            </v-row>
        </div>
     </div> 
 </template>
-
+<script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAq7abrbPQCp4oxf0jRCHIZZ5yMsphbB_8"></script>
 <script>
-    import { gmapsMap } from 'x5-gmaps'
+import ax from 'axios'
+export const axios = ax
 
 export default {
     name: "BaseRestInfo",
-    components: { gmapsMap },
     props: ['id'],
-    data: () => ({
-        mapOptions: {
-            center: { lat: -27.47, lng: 153.025 },
+
+    mounted: function(){
+        this.initMap();
+    },
+    methods: {
+        initMap() {
+            var myLatLng = {lat: -25.363, lng: 131.044};
+            var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 12,
+            center: myLatLng,
+            disableDefaultUI: true
+            });
+
+            this.getStreetAddressFrom(map)
+
         },
-    }),
+        async getStreetAddressFrom(map) {
+       try {
+           var address = this.restData.address + " " + this.restData.n_civ + " " + this.restData.city + " " + this.restData.cap;
+          var {
+             data
+          } = await axios.get("https://maps.googleapis.com/maps/api/geocode/json?address=" +
+              address + "&key=AIzaSyAq7abrbPQCp4oxf0jRCHIZZ5yMsphbB_8"
+          );
+          if (data.error_message) {
+             console.log(data.error_message)
+          } else {
+              var myLatLng = {lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng}
+              var marker = new google.maps.Marker({
+              position: myLatLng,
+              map: map,
+              title: this.restData.activity_name
+            });
+              map.setCenter(marker.getPosition());
+                var infowindow = new google.maps.InfoWindow({
+                    content: this.restData.activity_name
+                  });
+                infowindow.open(map, marker);
+               marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+          }
+       } catch (error) {
+          console.log(error.message);
+       }
+    }
+    },
     computed: {
         restData() {
             return this.$store.getters["restaurantData/restData"];
