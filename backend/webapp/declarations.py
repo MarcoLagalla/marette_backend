5cd42b807677dde3b4754ed8786c64491ffa1a0a
Marco Lagalla <marco.lagalla01@universitadipavia.it>
2020-06-17 21:58:09 +0200

    Update declarations.py

--- backend/webapp/declarations.py
+++ backend/webapp/declarations.py
@@ -74,13 +74,13 @@ RESTAURANT_CATEGORY_CHOICES = [
 ]
 
 DAYS = [
-    (1, 'Lunedi'),
-    (2, 'Martedi'),
-    (3, 'Mercoledi'),
-    (4, 'Giovedi'),
-    (5, 'Venerdi'),
-    (6, 'Sabato'),
-    (7, 'Domenica'),
+    ('1', 'Lunedi'),
+    ('2', 'Martedi'),
+    ('3', 'Mercoledi'),
+    ('4', 'Giovedi'),
+    ('5', 'Venerdi'),
+    ('6', 'Sabato'),
+    ('7', 'Domenica'),
 ]
 
 
