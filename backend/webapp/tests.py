81816a3bf54606d032a356482b8170f7ced3c2b2
Michele <michelereina@live.it>
2020-06-30 17:07:15 +0200

    added search test of restaurant according to query

--- backend/webapp/tests.py
+++ backend/webapp/tests.py
@@ -228,6 +228,10 @@ class RestaurantProductsTestCase(APITestCase):
 
         c1 = Category.objects.create(category_name="bar")
         c2 = Category.objects.create(category_name="pizzeria")
+
+        ProductTag.objects.create(name="Vegano", description="Cibo vegano, no carne")
+        ProductTag.objects.create(name="Gluten Free", description="Senza glutine")
+
         self.base_restaurant = Restaurant.objects.create(owner_id=self.base_business.pk, **rest_base_data)
         self.base_restaurant.restaurant_category.set([1, 2])
         self.base_restaurant.set_url()
@@ -317,6 +321,69 @@ class RestaurantProductsTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], "Pizza diavola")
         self.assertEqual(response.data['price'], "6.50")
+
+
+class RestaurantSearchTestCase(APITestCase):
+
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(username='admin', email='admin@gmail.com', password='1234')
+
+        #base customer
+        self.base_user = User.objects.create(username='mike', first_name='Mike', last_name='Tyson', email='test@test.app',
+                                        password=make_password('12345'))
+        cust_user_data = {"birth_date": "1994-04-20", "phone": "3458926930"}
+        self.base_customer = Customer.objects.create(user=self.base_user, **cust_user_data)
+        self.base_customer_token = Token.objects.create(user=self.base_user)
+
+        #base_business
+        self.base_user_b = User.objects.create(username='mikeB', first_name='MikeB', last_name='TysonB',
+                                          email='testB@test.app', password=make_password('12345'))
+        base_user_b_activation_token = account_activation_token.make_token(user=self.base_user_b)
+
+        buss_user_data = {"birth_date": "1994-04-20", "phone": "3458926930", "cf": "FRNGTN08R44L219V",
+                          "city": "Pavia", "address": "marconi nuova", "n_civ": "25", "cap": "27100"}
+        self.base_business = Business.objects.create(user=self.base_user_b,
+                                                     activation_token=base_user_b_activation_token,**buss_user_data)
+        self.base_business_token = Token.objects.create(user=self.base_user_b)
+       # print(self.base_business_token)
+
+        # base_restaurant
+        # rest_base_data = {"activity_name": "Pizzeria Ancora", "activity_description": "Tutto buonissimo",
+        #                   "city": "Pavia", "address": "marconi nuova", "n_civ": "25", "cap": "27100",
+        #                   "p_iva": "IT01766920761", "restaurant_number": "3456765789"}
+
+        c1 = Category.objects.create(category_name="bar")
+        c2 = Category.objects.create(category_name="pizzeria")
+        # self.base_restaurant = Restaurant.objects.create(owner_id=self.base_business.pk, **rest_base_data)
+        # self.base_restaurant.restaurant_category.set([1, 2])
+        # self.base_restaurant.set_url()
+
+        self.datastr = '{\n\t"activity_name": "Bella napoli", \n\t"activity_description": "Tutta la pizza che vuoi", ' \
+                       '\n\t"p_iva": "04113940409", \n\t"restaurant_number": "3456765689", ' \
+                       '\n\t"city": "milano", \n\t"address": "marconi nuova", \n\t"n_civ": "2", \n\t"cap": "27100", ' \
+                       '\n\t"restaurant_category": '
+
+    def test_business_can_activate_email(self):
+        response = self.client.get(reverse('account:activate_email', kwargs={'id': self.base_user_b.id,
+                                                                             'token': str(
+                                                                                 self.base_business.activation_token)}))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['activation'], 'Indirizzo email confermato, account attivo.')
+
+
+    def test_business_can_create_restaurant(self):
+        # Need to activate the email first
+        self.test_business_can_activate_email()
+
+        # Register the restaurants
+        query_dict = QueryDict('', mutable=True)
+        query_dict.update({'data': self.datastr + str([1,2]) + '}'})
+
+        self.client.force_login(self.base_user_b)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + str(self.base_business_token))
+        response = self.client.post(reverse('webapp:register_restaurant'), data=query_dict)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
 
