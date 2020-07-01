b2f821b5f325114b8fd8682e2e0a51f3bbf6b9cd
Marco Lagalla <marco.lagalla01@universitadipavia.it>
2020-07-01 16:10:39 +0200

    Update unzipBundle.sh

--- scripts/unzipBundle.sh
+++ scripts/unzipBundle.sh
@@ -2,11 +2,4 @@
 HOME_DIR=/home/marette
 WORKING_DIR=/home/marette/marette_backend
 
-sudo -i
 
-source $HOMEDIR/venv/bin/activate
-cd $WORKING_DIR
-
-yarn build
-python3 manage.py makemigrations
-python3 manage.py migrate
